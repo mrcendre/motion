4b86b698eb0e8337171cd3cdf58fc85a49827888
gcendre <gcendre@cabassegroup.com>
2022-08-05 00:42:57 +0200

    fix: v1.3.1 Handle lack of event and method channels in motion_platform_interface

--- motion_platform_interface/lib/src/native_motion.dart
+++ motion_platform_interface/lib/src/native_motion.dart
@@ -3,16 +3,16 @@
 // found in the LICENSE file.
 
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
 import 'package:motion_platform_interface/motion_platform_interface.dart';
 
 /// The native implementation of the [MotionPlatform] interface that uses a [MethodChannel] and an [EventChannel].
 class NativeMotion extends MotionPlatform {
-  static const EventChannel _gyroscopeEventChannel =
-      EventChannel('me.cendre.motion/gyroscope');
+  static EventChannel? _gyroscopeEventChannel;
 
-  static const MethodChannel _methodChannel = MethodChannel('me.cendre.motion');
+  static MethodChannel? _methodChannel;
 
   static Stream<MotionEvent>? _gyroscopeStream;
 
@@ -33,22 +33,34 @@ class NativeMotion extends MotionPlatform {
   FramesPerSecond _updateInterval = defaultUpdateInterval;
 
   @override
-  Stream<MotionEvent> get gyroscopeStream {
-    _gyroscopeStream ??=
-        _gyroscopeEventChannel.receiveBroadcastStream().map((dynamic event) {
-      final list = event.cast<double>();
-      return MotionEvent(
-          type: MotionType.gyroscope, x: list[0]!, y: list[1]!, z: list[2]!);
-    });
-    return _gyroscopeStream!;
+  Stream<MotionEvent>? get gyroscopeStream {
+    try {
+      _gyroscopeStream ??= _gyroscopeEventChannel?.receiveBroadcastStream().map((dynamic event) {
+        final list = event.cast<double>();
+        return MotionEvent(type: MotionType.gyroscope, x: list[0]!, y: list[1]!, z: list[2]!);
+      });
+      return _gyroscopeStream;
+    } catch (e) {
+      // If a PlatformException is thrown, the plugin is not available on the device.
+      _isGyroscopeAvailable = false;
+      return null;
+    }
   }
 
   @override
   Future<void> initialize() async {
-    final isEmpty = await gyroscopeStream.isEmpty;
+    if (Platform.isIOS || Platform.isAndroid) {
+      _methodChannel ??= MethodChannel('me.cendre.motion');
+      _gyroscopeEventChannel ??= EventChannel('me.cendre.motion/gyroscope');
 
-    // If the stream does not exist or is empty.
-    _isGyroscopeAvailable = !isEmpty;
+      final isEmpty = await gyroscopeStream?.isEmpty ?? true;
+
+      // If the stream does not exist or is empty.
+      _isGyroscopeAvailable = !isEmpty;
+    } else if (Platform.isMacOS || Platform.isLinux || Platform.isWindows) {
+      // Desktop platforms should not use the gyroscope events.
+      _isGyroscopeAvailable = false;
+    }
 
     return;
   }
@@ -59,7 +71,6 @@ class NativeMotion extends MotionPlatform {
   @override
   void setUpdateInterval(FramesPerSecond updateInterval) {
     _updateInterval = updateInterval;
-    _methodChannel.invokeMethod(
-        'setUpdateInterval', _updateInterval.inMicroseconds);
+    _methodChannel?.invokeMethod('setUpdateInterval', _updateInterval.inMicroseconds);
   }
 }
