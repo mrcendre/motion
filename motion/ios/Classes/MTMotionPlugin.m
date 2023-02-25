923677900c420b808473162492d15a6aed6967de
gcendre <gcendre@cabassegroup.com>
2023-02-25 12:44:00 +0100

    fix: Crash after detaching from FlutterEngine on iOS (#13)

--- motion/ios/Classes/MTMotionPlugin.m
+++ motion/ios/Classes/MTMotionPlugin.m
@@ -24,85 +24,119 @@ static BOOL _isGyroscopeAvailable()
 
 }
 
-
-@interface MTMotionPlugin ()
-
-@property (nonatomic, strong) FlutterMethodChannel* methodChannel;
-
-@end
-
 @implementation MTMotionPlugin
 
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
-  MTGyroscopeStreamHandler *gyroscopeStreamHandler =
-      [[MTGyroscopeStreamHandler alloc] init];
-  FlutterEventChannel *gyroscopeChannel = [FlutterEventChannel
-      eventChannelWithName:@"me.cendre.motion/gyroscope"
-           binaryMessenger:[registrar messenger]];
-  [gyroscopeChannel setStreamHandler:gyroscopeStreamHandler];
+NSMutableDictionary<NSString *, FlutterEventChannel *> *_eventChannels;
+NSMutableDictionary<NSString *, NSObject<FlutterStreamHandler> *> *_streamHandlers;
 
-  MTMotionPlugin* instance = [MTMotionPlugin new];
-  instance.methodChannel = [FlutterMethodChannel 
-      methodChannelWithName:@"me.cendre.motion" 
-            binaryMessenger:[registrar messenger]];
-  [registrar addMethodCallDelegate:instance channel:instance.methodChannel];
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
+    
+    //alloc channels names
+    _eventChannels = [NSMutableDictionary dictionary];
+    _streamHandlers = [NSMutableDictionary dictionary];
+    
+    MTMotionPlugin* instance = [MTMotionPlugin new];
+    
+    NSString* gyroscopeStreamHandlerName = @"me.cendre.motion/gyroscope";
+    MTGyroscopeStreamHandler* gyroscopeStreamHandler = [MTGyroscopeStreamHandler new];
+    [_streamHandlers setObject:gyroscopeStreamHandler forKey:gyroscopeStreamHandlerName];
+    
+    FlutterEventChannel* gyroscopeChannel = [FlutterEventChannel eventChannelWithName:gyroscopeStreamHandlerName
+                                                                      binaryMessenger:[registrar messenger]];
+    [gyroscopeChannel setStreamHandler:gyroscopeStreamHandler];
+    [_eventChannels setObject:gyroscopeChannel forKey:gyroscopeStreamHandlerName];
+    
+    FlutterMethodChannel* methodChannel = [FlutterMethodChannel methodChannelWithName:@"me.cendre.motion"
+                                                                      binaryMessenger:[registrar messenger]];
+    
+    [registrar addMethodCallDelegate:instance channel:methodChannel];
 }
+
+- (void)detachFromEngineForRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar{
+    _cleanUp();
+}
+
+static void _cleanUp(){
+    for (FlutterEventChannel *channel in _eventChannels.allValues) {
+        [channel setStreamHandler:nil];
+    }
+    [_eventChannels removeAllObjects];
+    for (NSObject<FlutterStreamHandler> *handler in _streamHandlers.allValues) {
+        [handler onCancelWithArguments:nil];
+    }
+    [_streamHandlers removeAllObjects];
+}
+
+
 
 - (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result
 {
     if ([@"setUpdateInterval" isEqualToString:call.method]) {
-      NSUInteger updateInterval = [call.arguments unsignedIntegerValue];
-      double sUpdateInterval = updateInterval / USEC_PER_SEC;
-
-      _motionManager.gyroUpdateInterval = sUpdateInterval;
-
-      result(@(YES));
-
-      return;
+        NSUInteger updateInterval = [call.arguments unsignedIntegerValue];
+        double sUpdateInterval = updateInterval / USEC_PER_SEC;
+        
+        _motionManager.gyroUpdateInterval = sUpdateInterval;
+        
+        result(@(YES));
+        
+        return;
     }
     
     if ([@"isGyroscopeAvailable" isEqualToString:call.method]) {
-      BOOL available = _isGyroscopeAvailable();
-      result(@(available));
-
-      return;
+        BOOL available = _isGyroscopeAvailable();
+        result(@(available));
+        
+        return;
     }
-
+    
     result(FlutterMethodNotImplemented);
 }
 
-
-
 @end
-
 
 static void sendTriplet(Float64 x, Float64 y, Float64 z,
                         FlutterEventSink sink) {
-  NSMutableData *event = [NSMutableData dataWithCapacity:3 * sizeof(Float64)];
-  [event appendBytes:&x length:sizeof(Float64)];
-  [event appendBytes:&y length:sizeof(Float64)];
-  [event appendBytes:&z length:sizeof(Float64)];
-  sink([FlutterStandardTypedData typedDataWithFloat64:event]);
+    //even if we removed all with [detachFromEngineForRegistrar] we can still receive and fire some events
+    //from sensors until detaching.
+    @try {
+        NSMutableData *event = [NSMutableData dataWithCapacity:3 * sizeof(Float64)];
+        [event appendBytes:&x length:sizeof(Float64)];
+        [event appendBytes:&y length:sizeof(Float64)];
+        [event appendBytes:&z length:sizeof(Float64)];
+        sink([FlutterStandardTypedData typedDataWithFloat64:event]);
+    }
+    @catch (NSException * e) {
+        NSLog(@"Error: %@ %@", e, [e userInfo]);
+    }
+    @finally {}
 }
 
 @implementation MTGyroscopeStreamHandler
 
 - (FlutterError *)onListenWithArguments:(id)arguments
                               eventSink:(FlutterEventSink)eventSink {
-  _initMotionManager();
-  [_motionManager
-      startGyroUpdatesToQueue:[[NSOperationQueue alloc] init]
-                  withHandler:^(CMGyroData *gyroData, NSError *error) {
-                    CMRotationRate rotationRate = gyroData.rotationRate;
-                    sendTriplet(rotationRate.x, rotationRate.y, rotationRate.z,
-                                eventSink);
-                  }];
-  return nil;
+    
+    _initMotionManager();
+    
+    [_motionManager startGyroUpdatesToQueue:[[NSOperationQueue alloc] init]
+                                withHandler:^(CMGyroData *gyroData, NSError *error) {
+        
+        CMRotationRate rotationRate = gyroData.rotationRate;
+        sendTriplet(rotationRate.x, rotationRate.y, rotationRate.z,
+                    eventSink);
+    }];
+    
+    return nil;
 }
 
 - (FlutterError *)onCancelWithArguments:(id)arguments {
-  [_motionManager stopGyroUpdates];
-  return nil;
+    [_motionManager stopGyroUpdates];
+    return nil;
+}
+
+- (void)dealloc
+{
+    _cleanUp();
 }
 
 @end
